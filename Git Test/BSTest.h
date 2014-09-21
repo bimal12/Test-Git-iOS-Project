593b4af71c3335594412317fc4e19355455623d0
Bimal Shah <bimalshah@bimalpc.home>
2014-09-21 16:15:14 +0100

    added a test class

---
+++ Git Test/BSTest.h
@@ -0,0 +1,13 @@
+//
+//  BSTest.h
+//  Git Test
+//
+//  Created by Bimal Shah on 21/09/2014.
+//  Copyright (c) 2014 Organsiation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSTest : NSObject
+
+@end
---
+++ Git Test/BSTest.m
@@ -0,0 +1,13 @@
+//
+//  BSTest.m
+//  Git Test
+//
+//  Created by Bimal Shah on 21/09/2014.
+//  Copyright (c) 2014 Organsiation. All rights reserved.
+//
+
+#import "BSTest.h"
+
+@implementation BSTest
+
+@end
