bd366dab02925d318cb211586a7d6c781e495924
Bimal Shah <bimalshah@bimalpc.home>
2014-09-21 15:54:40 +0100

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Bimal Shah on 21/09/2014.
+//  Copyright (c) 2014 Organsiation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Bimal Shah on 21/09/2014.
+//  Copyright (c) 2014 Organsiation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
